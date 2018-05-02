7e76612696720df9f78cade8e4ca86cff271263b
Cem Yusuf Aydogdu <cem.y.aydogdu@tum.de>
2018-05-02 15:55:37 +0200

    init DatasetKDD99 class

--- dataset.py
+++ dataset.py
@@ -12,3 +12,33 @@ class Dataset:
         self.folderpath = folderpath
 
 
+class DatasetKDD99(Dataset):
+
+    def __init__(self, folderpath):
+        Dataset.__init__(self, folderpath)
+        self.loadDataset()
+        self.enumerateStringColumns()
+
+    def loadDataset(self):
+        print("Preparing dataset:", self.folderpath)
+
+        ### load data and labels (normal and different attack types)
+        self.df_data = pd.read_csv(self.folderpath + "kddcup.data_10_percent.gz", header=None)
+        self.df_labels = pd.DataFrame(self.df_data.iloc[:, -1])  ### last column has labels
+        self.df_data = self.df_data.iloc[:, :-1]  ### columns until last column
+
+        ### read column names and types
+        col_names = []
+        col_datatypes = []
+
+        with open(self.folderpath + "kddcup.names") as file:
+            next(file)  ### skip first line
+            for line in file:
+                name, datatype = line.split(": ")
+                col_names.append(name)
+                col_datatypes.append(datatype.replace(".\n", ""))
+
+        self.df_data.columns = col_names
+        self.df_labels.columns = ["labels"]
+
+    ### TODO
